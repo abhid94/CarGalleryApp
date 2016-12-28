6be82b19b2fd9d41501db33b23721c9083a1eca0
Abhi Dutta <abhid94@gmail.com>
2016-12-28 14:29:25 +1100

    Added initial Xcode project

---
+++ CarGalleryApp/CarGalleryApp/FirstViewController.swift
@@ -0,0 +1,25 @@
+//
+//  FirstViewController.swift
+//  CarGalleryApp
+//
+//  Created by Abhi Dutta on 28/12/16.
+//  Copyright © 2016 abhiduttasolutions. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
